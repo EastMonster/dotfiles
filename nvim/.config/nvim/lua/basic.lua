bdc350d3542ba10269a2211877b8fd2c3838fe49
EastMonster <east_monster@126.com>
2023-09-08 10:05:43 +0800

    nvim: set clipboard to `win32yank.exe` (`unnamedplus`)

--- nvim/.config/nvim/lua/basic.lua
+++ nvim/.config/nvim/lua/basic.lua
@@ -5,3 +5,7 @@ vim.o.expandtab = true
 
 -- 行号
 vim.wo.number = true
+
+-- 剪贴板
+-- sudo ln -s /mnt/c/Program\ Files/Neovim/bin/win32yank.exe /usr/local/bin/win32yank.exe
+vim.g.clipboard = unnamedplus
